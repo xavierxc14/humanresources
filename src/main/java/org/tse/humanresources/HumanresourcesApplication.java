2cfdd6bb94d3536eac5573a0bc8184dae9690a36
Xavier Ñauñay <xavierxc14@gmail.com>
2017-10-11 18:27:39 +0200

    Initial commit

---
+++ src/main/java/org/tse/humanresources/HumanresourcesApplication.java
@@ -0,0 +1,12 @@
+package org.tse.humanresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesApplication.class, args);
+	}
+}
